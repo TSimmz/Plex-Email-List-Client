b9505feb4d02876852e27b5a7bd6a38a4260527d
tsimoni <tyler.simoni.8@gmail.com>
2018-01-19 10:59:24 -0600

    Added Add/Remove GUI and VMs

---
+++ DatPlex/ViewModel/Add_UserVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatPlex.ViewModel
+{
+    public class Add_UserVM
+    {
+        public Add_UserVM()
+        {
+
+        }
+    }
+}
